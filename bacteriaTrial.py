d334ee8897ce4660ccb5e68befbf917cc8baa76a
ggichuru <ggichuru.dev@gmail.com>
2021-05-23 16:49:43 +0300

    Owlready onto with python intro

--- bacteriaTrial.py
+++ bacteriaTrial.py
@@ -26,3 +26,27 @@ print(issubclass(onto.Coccus, onto.Bacterium))
 for sc in onto.Bacterium.subclasses():
     print(f'{sc.name} is a subclass of Bacterium class')
 print(onto.Bacterium.descendants(include_self = False))
+print()
+print(onto.Coccus.is_a)
+
+# change the name of property
+onto.has_grouping.python_name = "groupings"
+print(onto.unknown_bacterium.groupings, '\n')
+
+# Searching for entities            #NB:/ is case sensitive by default
+print(onto.search(iri = "*Coccus*"))                    #search all entities whose iri contains Coccus 
+print(onto.search(iri = "*Coccus*", _case_sensitive = False))
+r = onto.search(iri = "*Coccus*", _case_sensitive = False)  #lazy list
+print(r)
+# search multiple parameters
+s = onto.search(type = onto.Bacterium, gram_positive = True)
+print(s)
+print(onto.search(type = onto.Bacterium, gram_positive = "*"))     #search for a bacteria whose gram status is known
+# search for individuals having no relation
+print(onto.search(type = onto.Bacterium, has_shape = None))
+# search all ontologies incase multiple have been loaded
+print(default_world.search(iri="*Coccus*"))
+# Nested search
+s = onto.search(type = onto.Bacterium, has_grouping = onto.search(type = onto.InChain))
+# search_one is used to return one result instead of a list
+print(onto.search_one(iri = "*Coccus*", _case_sensitive = False))
